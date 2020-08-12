f08c380169271f9911b033e30bffd1bf44b982e5
Trai Nguyen <tmnguyen8@gmail.com>
2020-08-11 22:21:37 -0400

    adding server and failure/ success pages

--- app.js
+++ app.js
@@ -3,7 +3,67 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const port = 3000;
 
-app.listen( port, ()=> console.log(`Server is running on port ${port}.`))
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("", (req, res) => {
+    console.log(req.body)
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = `https://us17.api.mailchimp.com/3.0/lists/9f6dedd93d`;
+
+    const options = {
+        method: "POST",
+        auth: "tmnguyen8:feb90b1e950e3d59f52e994f654d0fa8-us17"
+    }
+
+    // Making HTTPS REQUEST
+    const request = https.request(url, options, (response) => {
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", (data) => {
+            console.log(JSON.parse(data))
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+})
+
+app.listen( port, ()=> console.log(`Server is running on port ${port}.`));
+
+// API MAILCHIMP KEY
+// feb90b1e950e3d59f52e994f654d0fa8-us17
+// MAILCHIMP ID
+// 9f6dedd93d
